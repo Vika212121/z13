5a42b40c4c3397f9c57a47f7f8cca0e8e289fc65
Julia Zhdanova <tratata7.89@mail.ru>
2022-08-24 09:47:57 +0300

    tests passed 5

--- src/main/java/ProductRepository.java
+++ src/main/java/ProductRepository.java
@@ -2,29 +2,14 @@ public class ProductRepository {
     private Product[] products = new Product[0];
 
     public void save(Product product) {
-        int length = products.length + 1;
-        Product[] tmp = new Product[length];
+        Product[] tmp = new Product[products.length + 1];
         System.arraycopy(products, 0, tmp, 0, products.length);
-        int index = tmp.length - 1;
-        tmp[index] = product;
+        tmp[tmp.length - 1] = product;
         products = tmp;
     }
 
     public Product[] findAll() {
         return products;
-    }
-
-    public void removeById(int id) {
-        int length = products.length - 1;
-        Product[] tmp = new Product[length];
-        int index = 0;
-        for (Product product : products) {
-            if (product.getId() != id) {
-                tmp[index] =product;
-                index++;
-            }
-        }
-        products = tmp;
     }
 
     public Product findById(int id) {
@@ -35,4 +20,20 @@ public class ProductRepository {
         }
         return null;
     }
+
+    public void removeById(int id) {
+        if (findById(id) == null) {
+            throw new RuntimeException("Такой id не существует: " + id);
+        }
+        Product[] tmp = new Product[products.length - 1];
+        int copyToIndex = 0;
+        for (Product product : products) {
+            if (product.getId() != id) {
+                tmp[copyToIndex] = product;
+                copyToIndex++;
+            }
+        }
+        products = tmp;
+    }
+
 }
